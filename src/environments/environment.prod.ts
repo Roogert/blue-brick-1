aa91191d8c4ffa7027dc05403ff90f8e97673357
Roogert <samrellergert@gmail.com>
2022-06-21 21:29:55 -0500

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_REBRICKABLE_TOKEN: '&key=30dde2f73200e1b1c5406c6016f65bd2',
+  API_FIREBASE_TOKEN:'AIzaSyBQV40RWI5AXFpgKnu-3nKxukFddVvd3yk'
 };
